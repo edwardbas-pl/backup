9578f44a97bbfc452ed2c645b6a9907a7dc3b8c2
edwardbas-pl <edwardbas551@interia.pl>
2020-04-26 12:01:17 +0200

    updated polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,9 +4,9 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
-polybar cos &
-polybar main &
+#polybar example &
+#polybar cos &
+#polybar main &
 
 
 
@@ -21,3 +21,10 @@ i3 gaps inner all set 15
 killall picom
 picom &
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
