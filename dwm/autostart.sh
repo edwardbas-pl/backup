f93028324eaf39e2db644bb8c07c062a528ad754
edwardbas-pl <dwardbas551@interia.pl>
2021-04-19 21:17:42 +0200

    update

---
+++ dwm/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wal -R &
+sh $HOME/.fehbg
+picom -bcCGfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.88 &
+dwm-status &
+xss-lock --transfer-sleep-lock -- betterlockscreen -l &
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
+setxkbmap -layout pl
